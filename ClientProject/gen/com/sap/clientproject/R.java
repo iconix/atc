93162210c80cb08bea1b278241e880982c5cd797
minhthao <minhthao@cs.stanford.edu>
2013-03-10 13:20:25 -0700

    map activity integrated

--- ClientProject/gen/com/sap/clientproject/R.java
+++ ClientProject/gen/com/sap/clientproject/R.java
@@ -26,24 +26,27 @@ public final class R {
         public static final int sap_logo=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000c;
+        public static final int action_settings=0x7f08000e;
         public static final int btnLogin=0x7f080005;
-        public static final int btnRegister=0x7f08000a;
+        public static final int btnRegister=0x7f08000c;
         public static final int header=0x7f080001;
-        public static final int link_to_login=0x7f08000b;
+        public static final int link_to_login=0x7f08000d;
         public static final int link_to_register=0x7f080006;
         public static final int loginErrorMessage=0x7f080002;
         public static final int loginUserID=0x7f080003;
         public static final int loginUserPassword=0x7f080004;
-        public static final int reg_email=0x7f080008;
-        public static final int reg_name=0x7f080007;
-        public static final int reg_password=0x7f080009;
+        public static final int map=0x7f080007;
+        public static final int reg_email=0x7f08000a;
+        public static final int reg_name=0x7f080009;
+        public static final int reg_password=0x7f08000b;
+        public static final int registerErrorMessage=0x7f080008;
         public static final int splashscreen=0x7f080000;
     }
     public static final class layout {
         public static final int activity_client_main=0x7f030000;
         public static final int login=0x7f030001;
-        public static final int register=0x7f030002;
+        public static final int map=0x7f030002;
+        public static final int register=0x7f030003;
     }
     public static final class menu {
         public static final int client_main=0x7f070000;
