b0d9325ebbd50b1cf0112a58cf6866f2b4ec7793
iconix <narhodes1@gmail.com>
2013-03-10 05:16:06 +0000

    Remove old directories

--- rails_projects/business_app/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-BusinessApp::Application.config.secret_token = '073db2d59c8b7b23888493aaeadb8976f2dbea6f0628f219e0d850c6155228ab56378a2acc8b939f845e22eeabd9078abf7fc7293c30fc1357606d40d91daf83'
