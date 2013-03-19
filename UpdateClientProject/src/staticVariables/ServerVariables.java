adc1211c842a402a7f87542a9da5ba5f679128ed
minhthao <minhthao@cs.stanford.edu>
2013-03-18 23:53:11 -0700

    With Nadja help, the server now run on the Amazon host.

--- UpdateClientProject/src/staticVariables/ServerVariables.java
+++ UpdateClientProject/src/staticVariables/ServerVariables.java
@@ -1,5 +1,6 @@
 package staticVariables;
 
 public class ServerVariables {
+    //url for the server
     public static String URL = "http://javawebappenv-7rmrmnw2am.elasticbeanstalk.com/ServerServlet";
 }
