e8f87c392302e86dd88fee5b37403f531ef4d720
iconix <narhodes1@gmail.com>
2013-03-19 02:19:08 -0700

    Update DBConnection, now works with Amazon RDS

--- ServerGps/src/db/DBConnection.java
+++ ServerGps/src/db/DBConnection.java
@@ -3,32 +3,28 @@ package db;
 import java.sql.*;
 /**
  * ssh -i smartsense.pem ubuntu@ec2-54-241-229-70.us-west-1.compute.amazonaws.com
- * mysql ebdb -h aauvtj3qaokqef.cxstmaj8fdow.us-west-1.rds.amazonaws.com -P 3306 -u sapenguins -p 
- * sapenguins
+ * Amazon DB: mysql ebdb -h aauvtj3qaokqef.cxstmaj8fdow.us-west-1.rds.amazonaws.com -P 3306 -u sapenguins -p 
+ * Amazon DB password: sapenguins
  * mysql c_cs108_minhthao -h mysql-user.stanford.edu -u ccs108minhthao -p
  * Handing all the accesses to the DB
  */
 
 public class DBConnection {
 	
-	/*
 	private static final String MYSQL_USERNAME = "sapenguins";
 	private static final String MYSQL_PASSWORD = "sapenguins";
-	private static final String MYSQL_DATABASE_SERVER = "aauvtj3qaokqef.cxstmaj8fdow.us-west-1.rds.amazonaws.com";
+	private static final String MYSQL_DATABASE_SERVER = "aauvtj3qaokqef.cxstmaj8fdow.us-west-1.rds.amazonaws.com:3306";
 	private static final String MYSQL_DATABASE_NAME = "ebdb";
-	private static final String MYSQL_DATABASE_PORT = "3306";
-	*/
-	private static final String MYSQL_USERNAME = "ccs108minhthao";
+	
+	/*private static final String MYSQL_USERNAME = "ccs108minhthao";
 	private static final String MYSQL_PASSWORD = "moulohth";
 	private static final String MYSQL_DATABASE_SERVER = "mysql-user.stanford.edu";
-	private static final String MYSQL_DATABASE_NAME = "c_cs108_minhthao";
+	private static final String MYSQL_DATABASE_NAME = "c_cs108_minhthao";*/
+	
 	private static Connection con;
 	static {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			/*String url = "jdbc:mysql://" + MYSQL_DATABASE_SERVER + ":" +
-					MYSQL_DATABASE_PORT +"/" + MYSQL_DATABASE_NAME;
-					*/
 			String url = "jdbc:mysql://" + MYSQL_DATABASE_SERVER + "/" + MYSQL_DATABASE_NAME;
 			con = DriverManager.getConnection(url, MYSQL_USERNAME, MYSQL_PASSWORD);
 		} catch (SQLException e) {
