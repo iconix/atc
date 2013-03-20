f1ca49b44e75ba442b8cedb9c2fab17064a7c678
iconix <narhodes1@gmail.com>
2013-03-20 11:32:06 +0000

    Updated routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,24 @@
 BusinessApp::Application.routes.draw do
+  #resources :businesses
+
+  #root to: 'static_pages#home'
+
+  #match 'signup', to: 'businesses#new'
+
   resources :businesses
-
+  resources :sessions, only: [:new, :create, :destroy]
+ 
+  match '/signup',  to: 'businesses#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
+ 
   root to: 'static_pages#home'
-
-  match 'signup', to: 'businesses#new'
+ 
+  match '/signup',  to: 'businesses#new'
+ 
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
