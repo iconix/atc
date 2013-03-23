7ab1e3715d8321b4bcfd8e231423ae1626921a92
Darin Im <darinim@stanford.edu>
2013-03-22 23:54:01 -0700

    Last Commit before Deadline
    Nothing substantial

--- AroundTheCorner/src/com/example/aroundthecorner/RecentAdActivity.java
+++ AroundTheCorner/src/com/example/aroundthecorner/RecentAdActivity.java
@@ -1,5 +1,7 @@
 package com.example.aroundthecorner;
 
+import java.util.ArrayList;
+
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.widget.ExpandableListView;
@@ -10,12 +12,34 @@ import android.view.View;
 public class RecentAdActivity extends ListActivity {
 	
 	private ExpandableListView expandableList;
+	private RecentAdAdapter adapter;
 	
 	static final String[] RECENT_LIST = new String[] {"Baume", "Garden Fresh", "Tamarine"};
 	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.recent_ads_layout);
 		
+		ArrayList<DealsDates> datesList = new ArrayList<DealsDates>();
+		expandableList = (ExpandableListView) findViewById(R.id.recent_list);
+		adapter = new RecentAdAdapter(getApplicationContext(), datesList);
+		expandableList.setAdapter(adapter);
 	}
+	
+	private void expandAll() {
+		int count = adapter.getGroupCount();
+		for (int i = 0; i < count; i++) {
+			expandableList.expandGroup(i);
+		}
+	}
+	
+	private void collapseAll() {
+		int count = adapter.getGroupCount();
+		for (int i = 0; i < count; i++) {
+			expandableList.collapseGroup(i);
+		}
+	}
+	
+	
 
 }
