2e6850685b65892e7e1b2ef429cff60969b926b4
iconix <narhodes1@gmail.com>
2013-05-08 11:16:30 -0700

    Update migration again

--- db/migrate/20130508174310_remove_web_businesses_foreign_keys.rb
+++ db/migrate/20130508174310_remove_web_businesses_foreign_keys.rb
@@ -1,5 +1,5 @@
 class RemoveWebBusinessesForeignKeys < ActiveRecord::Migration
   def change
-  	remove_column :deals, :web_business_id if column_exists?(:deals)
+  	remove_column :deals, :web_business_id if column_exists?(:deals, :web_business_id)
   end
 end
