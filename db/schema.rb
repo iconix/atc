3815dda1313825683ebb37f4fa5ad3098a5c93e6
cfierro <carlosfierro66@gmail.com>
2013-05-12 13:33:34 -0700

    Changed schema back to normal

--- db/schema.rb
+++ db/schema.rb
@@ -20,16 +20,15 @@ ActiveRecord::Schema.define(:version => 20130508182512) do
     t.string   "remember_token"
     t.string   "websiteURL"
     t.string   "imageURL"
-    t.binary   "imageUpload",        :limit => 2097152
-    t.string   "shortDescription"
-    t.string   "longDescription",    :limit => 1024
-    t.decimal  "latitude",                              :precision => 15, :scale => 10
-    t.decimal  "longitude",                             :precision => 15, :scale => 10
+    t.text     "shortDescription"
+    t.text     "longDescription"
+    t.decimal  "latitude",           :precision => 15, :scale => 10
+    t.decimal  "longitude",          :precision => 15, :scale => 10
     t.text     "address"
-    t.integer  "phoneNumber"
-    t.datetime "created_at",                                                                               :null => false
-    t.datetime "updated_at",                                                                               :null => false
-    t.boolean  "admin",                                                                 :default => false
+    t.string   "phoneNumber"
+    t.datetime "created_at",                                                            :null => false
+    t.datetime "updated_at",                                                            :null => false
+    t.boolean  "admin",                                              :default => false
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
@@ -41,26 +40,25 @@ ActiveRecord::Schema.define(:version => 20130508182512) do
 
   create_table "deals", :force => true do |t|
     t.integer  "business_id"
-    t.decimal  "longitude",                             :precision => 15, :scale => 10
-    t.decimal  "latitude",                              :precision => 15, :scale => 10
+    t.decimal  "longitude",          :precision => 15, :scale => 10
+    t.decimal  "latitude",           :precision => 15, :scale => 10
     t.datetime "startDate"
     t.datetime "endDate"
     t.string   "title"
     t.string   "imageURL"
-    t.binary   "imageUpload",        :limit => 2097152
-    t.string   "shortDescription"
-    t.text     "longDescription",    :limit => 1024
+    t.text     "shortDescription"
+    t.text     "longDescription"
     t.string   "tags"
     t.text     "address"
-    t.boolean  "isEvent",                                                               :default => false
-    t.datetime "created_at",                                                                               :null => false
-    t.datetime "updated_at",                                                                               :null => false
+    t.boolean  "isEvent",                                            :default => false
+    t.datetime "created_at",                                                            :null => false
+    t.datetime "updated_at",                                                            :null => false
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
   end
 
-  add_index "deals", ["business_id", "created_at"], :name => "index_deals_on_business_id_and_created_at"
+  add_index "deals", ["business_id", "created_at"], :name => "index_deals"
 
 end
