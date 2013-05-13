4f92076400b7739155bf0edb7b697ab3e9ca9999
Toan Tran <toan@ubuntu.(none)>
2013-05-12 17:47:30 -0700

    add missed CSS for jQuery datetime

--- db/schema.rb
+++ db/schema.rb
@@ -20,15 +20,16 @@ ActiveRecord::Schema.define(:version => 20130508182512) do
     t.string   "remember_token"
     t.string   "websiteURL"
     t.string   "imageURL"
-    t.text     "shortDescription"
-    t.text     "longDescription"
-    t.decimal  "latitude",           :precision => 15, :scale => 10
-    t.decimal  "longitude",          :precision => 15, :scale => 10
+    t.binary   "imageUpload",        :limit => 2097152
+    t.string   "shortDescription"
+    t.string   "longDescription",    :limit => 1024
+    t.decimal  "latitude",                              :precision => 15, :scale => 10
+    t.decimal  "longitude",                             :precision => 15, :scale => 10
     t.text     "address"
-    t.string   "phoneNumber"
-    t.datetime "created_at",                                                            :null => false
-    t.datetime "updated_at",                                                            :null => false
-    t.boolean  "admin",                                              :default => false
+    t.integer  "phoneNumber"
+    t.datetime "created_at",                                                                               :null => false
+    t.datetime "updated_at",                                                                               :null => false
+    t.boolean  "admin",                                                                 :default => false
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
@@ -40,25 +41,26 @@ ActiveRecord::Schema.define(:version => 20130508182512) do
 
   create_table "deals", :force => true do |t|
     t.integer  "business_id"
-    t.decimal  "longitude",          :precision => 15, :scale => 10
-    t.decimal  "latitude",           :precision => 15, :scale => 10
+    t.decimal  "longitude",                             :precision => 15, :scale => 10
+    t.decimal  "latitude",                              :precision => 15, :scale => 10
     t.datetime "startDate"
     t.datetime "endDate"
     t.string   "title"
     t.string   "imageURL"
-    t.text     "shortDescription"
-    t.text     "longDescription"
+    t.binary   "imageUpload",        :limit => 2097152
+    t.string   "shortDescription"
+    t.text     "longDescription",    :limit => 1024
     t.string   "tags"
     t.text     "address"
-    t.boolean  "isEvent",                                            :default => false
-    t.datetime "created_at",                                                            :null => false
-    t.datetime "updated_at",                                                            :null => false
+    t.boolean  "isEvent",                                                               :default => false
+    t.datetime "created_at",                                                                               :null => false
+    t.datetime "updated_at",                                                                               :null => false
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
   end
 
-  add_index "deals", ["business_id", "created_at"], :name => "index_deals"
+  add_index "deals", ["business_id", "created_at"], :name => "index_deals_on_business_id_and_created_at"
 
 end
