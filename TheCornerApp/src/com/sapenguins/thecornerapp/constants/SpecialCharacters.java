5fe525d9211b4bb57a33f13d6c058342a7e61039
minhthao <minhthao@cs.stanford.edu>
2013-06-07 19:08:32 -0700

    final big update of the program that showcased during software fair

--- TheCornerApp/src/com/sapenguins/thecornerapp/constants/SpecialCharacters.java
+++
@@ -1,7 +0,0 @@
-package com.sapenguins.thecornerapp.constants;
-
-public class SpecialCharacters {
-	public static final String delimiter = "~~";
-	public static final String endLn = System.getProperty("line.separator");
-	public static final String empty = "###EMPTY###";
-}
